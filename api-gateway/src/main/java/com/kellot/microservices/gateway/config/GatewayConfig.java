4e70c6fdf8084e046edd105db66d8bb9de4b077d
Keev <thwaykyawsitt@yahoo.com>
2025-08-05 18:56:22 +0630

    feat: Added Service Discovery server
    - Eureka service discovery server is added.
    - All the services have been integrated with eureka client as well.
    - Endpoints can now be called through API gateway.

--- api-gateway/src/main/java/com/kellot/microservices/gateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/kellot/microservices/gateway/config/GatewayConfig.java
@@ -1,12 +1,32 @@
 package com.kellot.microservices.gateway.config;
 
+import io.netty.resolver.DefaultAddressResolverGroup;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class GatewayConfig {
+    @Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Or restrict to specific origins
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
@@ -14,15 +34,24 @@ public class GatewayConfig {
                 .route("order-service", r -> r.path("/orders/**")
                         .filters(f -> f.prefixPath("/api")
                                 .addResponseHeader("X-Powered-By", "KST Cloud Gateway for order service"))
-                        .uri("http://order_service:8080"))
+                        .uri("lb://order-service"))
                 .route("inventory-service", r -> r.path("/inventory/**")
                         .filters(f -> f.prefixPath("/api")
                                 .addResponseHeader("X-Powered-By", "KST Cloud Gateway for inventory service"))
-                        .uri("http://inventory_service:8080"))
+                        .uri("lb://inventory-service"))
                 .route("product-service", r -> r.path("/products/**")
                         .filters(f -> f.prefixPath("/api")
                                 .addResponseHeader("X-Powered-By", "KST Cloud Gateway for product service"))
-                        .uri("http://product_service:8080"))
+                        .uri("lb://product-service"))
                 .build();
     }
+
+//    @Bean
+//    @Primary
+//    public WebClient webClient() {
+//        HttpClient httpClient = HttpClient.create().resolver(DefaultAddressResolverGroup.INSTANCE);
+//        return WebClient.builder()
+//                .clientConnector(new ReactorClientHttpConnector(httpClient))
+//                .build();
+//    }
 }
--- order-service/src/main/java/com/kellot/microservices/order/client/InventoryServiceClient.java
+++ order-service/src/main/java/com/kellot/microservices/order/client/InventoryServiceClient.java
@@ -5,7 +5,7 @@ import com.kellot.microservices.order.dto.InventoryResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-@FeignClient(value = "inventory-service", url = "http://localhost:8082/api/inventory")
+@FeignClient(value = "inventory-service", url = "${api.gateway.url}/api/inventory")
 public interface InventoryServiceClient {
     @GetMapping(value = "/{skuCode}")
     InventoryResponse getInventoryBySkuCode(@PathVariable("skuCode") String skuCode);
